7ce3244c06991d78205d49c63b3a201808028e80
ssssssg-hub <83156255+ssssssg-hub@users.noreply.github.com>
2022-10-21 18:55:51 +0900

    first commit

---
+++ src/shared/Router.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
