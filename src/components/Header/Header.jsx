3bcf85cf33288f3dc5814e6b133654f44b5932c5
ssssssg-hub <83156255+ssssssg-hub@users.noreply.github.com>
2022-10-22 03:38:10 +0900

    헤더, 회원가입 UI 작업

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="headerBox">
+      <a href="#" className="mainTitle">
+        홈트
+      </a>
+      <div className="categoryBox">
+        <a href="#">카테고리1</a> {/* a는 나중에 다 link로 변경*/}
+        <a href="#">카테고리2</a>
+      </div>
+      <div className="rightHeader">
+        <p>닉네임</p>
+        <a href="#">로그아웃</a>
+        <a href="#">마이페이지</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./join.css";
+
+const Join = () => {
+  return (
+    <div className="wrap">
+      <div className="Joinwrap">
+        <form className="inputform">
+          <h1>작심홈트</h1>
+          <input type="text" placeholder="이메일을 입력하세요"></input>
+          <button className="btn">중복확인</button>
+          <br></br>
+          <input type="text" placeholder="비밀번호를 입력하세요"></input>
+          <br></br>
+          <input type="text" placeholder="비밀번호를 재입력하세요"></input>
+          <br></br>
+          <button className="btn">회원가입</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
